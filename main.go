60594ce814b268209be081e1f73de2f558f083ad
abhigyandwivedi <abhigyan.dwivedi@gmail.com>
2019-04-21 04:37:50 +0530

    Hello Modules Project Init

--- main.go
+++ main.go
@@ -2,65 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func randEmit(ch chan int) {
-	id := 0
-
-	for {
-		ch <- id
-		id++
-		time.Sleep(500 * time.Millisecond)
-	}
-}
-
-func emit(ch chan string) {
-	words := []string{"the", "quick", "brown", "fox"}
-
-	for _, word := range words {
-		ch <- word
-		time.Sleep(20 * time.Millisecond)
-
-	}
-	close(ch)
-}
-
-func unitemit(ch chan string) {
-	ch <- "acme"
-	close(ch)
-}
-
 func main() {
-	/*	fmt.Println("Hello World")
-		ch := make(chan string)
-		ch2 := make(chan string)
-
-		go emit(ch)
-		go unitemit(ch2)
-
-		for word := range ch {
-			fmt.Print(" ", word)
-		}
-		close(ch)
-
-		fmt.Println(<-ch2)
-		str, ok := <-ch2
-		fmt.Println(str, ok)
-	*/
-	ch3 := make(chan int)
-	go randEmit(ch3)
-
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
-	fmt.Println(<-ch3)
+	fmt.Println("The main module was invoked!!!")
 
 }
--- models/message.go
+++ models/message.go
@@ -1,8 +1,4 @@
 package models
 
-// Message structures..........
 type Message struct {
-	TraceID string                 `json:"trace_id"`
-	Header  string                 `json:"header"`
-	Payload map[string]interface{} `json:"payload"`
 }
